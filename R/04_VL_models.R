e205bd7d712ea1011166807a39a4c982dc14bb31
Roman Link <romanlink@gmx.de>
2018-05-23 20:24:03 +0200

    Changed a large number of bugs (errors in data statement etc), added the silent = TRUE flag to all calls of try() and removed experimental section

--- R/04_VL_models.R
+++ R/04_VL_models.R
@@ -26,7 +26,7 @@
   # crude estimate of average vessel length: x intercept of line through first 
   # two counts (exluding counts that do not differ from first count)
   which <- c(1, which(counts < counts[1])[1])
-  coefs <- coef(lm(counts[which] ~ z[which]))
+  coefs <- as.numeric(coef(lm(counts[which] ~ z[which])))
   mu_start <- - coefs[1]/coefs[2]
   
   # return starting values depending on distribution
@@ -53,9 +53,7 @@
 # length distributions (based on the mle2 function from package bbmle)
 mle_VL <-   function(z, counts, dist, subs = FALSE, size = NULL, start, lower, 
                      control = list(eval.max = 999, iter.max = 999)){
-  # load bbmle package
-  require(bbmle)
-  
+
   # get function for the shape of the infusion profile p(z)
   p_dist <- eval(parse(text = paste0("p_", dist, "_mu")), envir = .GlobalEnv)
 
@@ -72,12 +70,10 @@ mle_VL <-   function(z, counts, dist, subs = FALSE, size = NULL, start, lower,
                  counts0 = counts[1:(m-1)],
                  p_dist = p_dist) # has to be included here due to the weird evaluation
                                   # tweaks used in bbmle
-    
     # get model formula
-    form <- eval(parse(text = paste0("counts1 ~ dbinom(size = counts0,
-                                                       prob = p_dist(z1, ", args, ") /
-                                                              p_dist(z0, ", args, "))")))
-    cat(deparse(form))
+    form <- eval(parse(text = paste0("counts1 ~ dbinom(size = counts0, prob = p_dist(z1, ", args, ") /
+                                                       p_dist(z0, ", args, "))")))
+    
     # fit model
     mod <- bbmle::mle2(minuslogl = form,
                        data = data,
@@ -92,8 +88,8 @@ mle_VL <-   function(z, counts, dist, subs = FALSE, size = NULL, start, lower,
                                                       prob = p_dist(z, ", args, "))")))
     # fit model
     mod <- bbmle::mle2(minuslogl = form,
-                       data = list(counts = counts, distances = distances,
-                                   size = size, p_dist= p_dist), # for p_dist see above
+                       data = list(counts = counts, z = z,
+                                   size = size, p_dist = p_dist), # for p_dist see above
                        optimizer = "nlminb",
                        start = start,
                        lower = lower,
@@ -111,25 +107,26 @@ mle_VL <-   function(z, counts, dist, subs = FALSE, size = NULL, start, lower,
 # the amount of failed models, these models are re-run 10 times with different 
 # starting values
 
-fit_mod <- function(type, z, counts, dist, subs, size){
+fit_mod <- function(type, z, counts, subs, size){
   # get initial starting values
-  start <- .get_start(z = z, counts = counts, dist = dist)
+  start <- .get_start(z = z, counts = counts, dist = type)
   # set counter
   t <- 1
   # set output value if model fails to NA
-  mod <- NA
+  mod <- NULL
   # fit cohen model
   if (type == "cohen"){
-    try(mod <- lm(log(counts[counts != 0]) ~ z[counts != 0]))
+    try(mod <- lm(log(counts[counts != 0]) ~ z[counts != 0]), silent = TRUE)
   }
+  
   # fit christman model
   if (type == "christman"){
     freq <- counts / size
     repeat {
       # try to fit model
-      try(mod <- nls(freq ~ exp(- (k * z) ^ c), start = start))
+      try(mod <- nls(freq ~ exp(- (k * z) ^ c), start = start), silent = TRUE)
       # break if model converged (second if clause checks whether it truly converged)
-      if (!is.na(mod)) break
+      if (!is.null(mod)) break
       # advance counter
       t <- t + 1
       # break if model did not converge after 10 trials
@@ -141,13 +138,13 @@ fit_mod <- function(type, z, counts, dist, subs, size){
   }
   
   # fit all other models
-  else{
-    lower <- .get_lower(dist = dist)
+  if (!(type %in% c("cohen", "christman") )) {
+    lower <- .get_lower(dist = type)
     repeat {
       try(mod <- mle_VL(z = z, counts = counts, dist = type, subs  = subs, 
-                        size = size, start = start, lower = lower))
+                        size = size, start = start, lower = lower), silent = TRUE)
       # break if model converged (second if clause checks whether it truly converged)
-      if (!is.na(mod)) {if (!is.na(summary(mod)@coef[2]) )  break}
+      if (!is.null(mod)) {if (!is.na(summary(mod)@coef[2]) )  break}
       # advance counter
       t <- t + 1
       # break if model did not converge after 10 trials
@@ -163,11 +160,11 @@ fit_mod <- function(type, z, counts, dist, subs, size){
 # 4. get_coefs() ---------------------------------------------------------------
 # function operator that gets the estimated average VL and other coefficients from each model
 get_coefs <- function(type, mod){
-  out <- c(mean_est = NA, par2_est = NA, par2_est_name = NA)
+  res <- c(mean_est = NA, par2_est = NA, par2_est_name = NA)
 
   # cohen model
   if (type == "cohen"){
-    out[1] <- -2/coef(mod)[2]
+    res[1] <- -2/coef(mod)[2]
   }
   
   # christman model
@@ -177,48 +174,19 @@ get_coefs <- function(type, mod){
     # minimum vessel length   
     lower <- ifelse(c < 1, 0, (1 / k) * ((c - 1) / c) ^ (1 / c))
     # average vessel length (by numerical integration)
-    out[1] <- integrate(function(x) x * Fchristman1(x, c, k), lower, Inf)$value # below minimum length, integral is 0
+    res[1] <- integrate(function(x) x * Fchristman1(x, c, k), lower, Inf)$value # below minimum length, integral is 0
     } 
   
   # all other models
-  else{ 
-    coefs  <- coefs(mod)
-    out[1] <- coefs[1]
+  if (!(type %in% c("cohen", "christman") )) {
+    coefs  <- coef(mod)
+    res[1] <- coefs[1]
     if (length(coefs) > 1){
-      out[2] <- coefs[2]
-      out[3] <- names(coefs)[2]
+      res[2] <- coefs[2]
+      res[3] <- names(coefs)[2]
       }
   }
   # return results
-  return(out)
+  return(res)
 }
-
-
-
-## important: starting value have to be in a list!!!
-
-### experimental
-# 
-# with(VLP, attach(list(z = distances, counts = counts, dist = "gamma", 
-#                  subs = FALSE, size = NULL, start = list(shape = 1, mu = 30), 
-#                  lower = list(shape = 0.001, mu = 0.001))))
-# 
-# mmm <- with(VLP, mle_VL(z = distances, counts = counts, dist = "erlang2", 
-#                         subs = FALSE, size = NULL, start = c(mu = 1), 
-#                         lower = c(mu = 0.000001)))
-# mmm
-# 
-# mmm <- with(VLP, mle_VL(z = distances, counts = counts, dist = "weibull", 
-#                         subs = FALSE, size = NULL,  start = list(shape = 1, mu = 2), 
-#                         lower = list(shape = 0.0001, mu = 0.0001)))
-# mmm
-# 
-# settings <- read.csv("settings/monte_carlo_settings.csv") %>% tbl_df
-# 
-# (settings1 <- settings[46711, ])
-# 
-# VLP  <- VL_profile(settings = settings1)
-# Vls  <- VL_subs(VLP, 50)
-# 
-
 
